22079eced0bea629fe0750079ae35bb60dcf574c
yashdhume <yash_dhume@yahoo.ca>
2019-02-10 03:18:25 -0500

    Finshed Story

--- lib/ui/StoryBoard/Story.dart
+++ lib/ui/StoryBoard/Story.dart
@@ -11,7 +11,24 @@ class StoryItem {
 }
 
 final sampleItems = <StoryItem>[
-  new StoryItem(title: '', category: '', imageUrl: 'assets/pictures/Story/1.png',),
-  new StoryItem(title: '', category: '', imageUrl: 'assets/pictures/Story/2.png',),
-  new StoryItem(title: '', category: '', imageUrl: 'assets/pictures/Story/3.png',),
+  new StoryItem(title: '', category: 'Brave The Future', imageUrl: 'assets/BTF_Disrupted_Logo_Pink_BG.jpg',),
+  new StoryItem(title: 'of Post Secondary Education', category: 'This is the Future', imageUrl: 'assets/pictures/Story/1.png',),
+  new StoryItem(title: 'Technology and Social Innovation', category: 'Inspired by Possibility', imageUrl: 'assets/pictures/Story/2.png',),
+  new StoryItem(title: 'Reimagined', category: 'This is Higher Learning', imageUrl: 'assets/pictures/Story/4.png',),
+  new StoryItem(title: 'Resilient Students', category: 'Led by', imageUrl: 'assets/pictures/Story/3.png',),
+  new StoryItem(title: 'Unstoppable', category: 'The', imageUrl: 'assets/pictures/Story/6.png',),
+  new StoryItem(title: 'Tomorrow', category: 'Disruptors of', imageUrl: 'assets/pictures/Story/7.png',),
+  new StoryItem(title: 'Forward', category: 'This is a Collective Charge', imageUrl: 'assets/pictures/Story/8.png',),
+  new StoryItem(title: 'Collaborative Research', category: 'Of Industry Standard', imageUrl: 'assets/pictures/Story/9.png',),
+  new StoryItem(title: 'State-of-the-art Facilities', category: 'Experiential Learning', imageUrl: 'assets/pictures/Story/10.png',),
+  new StoryItem(title: 'With Entrepreneurship', category: 'at its Core', imageUrl: 'assets/pictures/Story/11.png',),
+  new StoryItem(title: 'Opportunity to Lead', category: 'This is an Investment in Progress', imageUrl: 'assets/pictures/Story/12.png',),
+  new StoryItem(title: 'For Canada', category: 'This is a Bold Vision', imageUrl: 'assets/pictures/Story/13.png',),
+  new StoryItem(title: '', category: 'And the World', imageUrl: 'assets/pictures/Story/14.png',),
+  new StoryItem(title: '', category: '', imageUrl: 'assets/pictures/Story/15.png',),
+  new StoryItem(title: '', category: '', imageUrl: 'assets/pictures/Story/16.png',),
+
+
+
+
 ];
